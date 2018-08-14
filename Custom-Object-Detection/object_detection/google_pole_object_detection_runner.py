d44807ccfe48a225229c7895829e5cf0c6bf6e66
piripinui <phillipjamesrobinson@googlemail.com>
2018-08-14 16:09:13 +0100

    Set aspect ratio to portrait

--- Custom-Object-Detection/object_detection/google_pole_object_detection_runner.py
+++ Custom-Object-Detection/object_detection/google_pole_object_detection_runner.py
@@ -67,13 +67,14 @@ def detect_objects(image_path, sess, image_tensor, detection_boxes, detection_sc
         use_normalized_coordinates=True,
         line_thickness=8)
     fig = plt.figure()
-    fig.set_size_inches(16, 9)
+    #fig.set_size_inches(16, 9)
+    fig.set_size_inches(9, 16)
     ax = plt.Axes(fig, [0., 0., 1., 1.])
     ax.set_axis_off()
     fig.add_axes(ax)
 
     #plt.imshow(image_np, aspect = 'auto')
-    plt.imshow(image_np)
+    plt.imshow(image_np, aspect = 'equal')
     path, file = os.path.split(image_path)
     plt.savefig(PATH_TO_TEST_IMAGES_DIR + '/processed/' + file, dpi = 62)
     plt.close(fig)
