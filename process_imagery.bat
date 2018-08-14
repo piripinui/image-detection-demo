ad2a99901809b662a044894d3f95b8860b710b51
piripinui <phillipjamesrobinson@googlemail.com>
2018-08-14 14:32:17 +0100

    Remove extra files

--- process_imagery.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-D:
-cd \Custom-Object-Detection
-
-set PYTHONPATH=d:\Custom-Object-Detection;D:\Custom-Object-Detection\slim
-python object_detection\google_pole_object_detection_runner.py
