9328bdc084b3d07af12039e87c3749f27960d457
piripinui <phillipjamesrobinson@googlemail.com>
2018-09-11 10:44:08 +0100

    Added port parameter

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const ImageDetectionProcessor = require('./ImageDetectionProcessor.js'),
 commandLineArgs = require('command-line-args'),
 optionDefinitions = [
-	  { name: 'imagedir', alias: 'd', type: String, defaultValue: './images' },
-	  { name: 'saveimagedata', alias: 's', type: Boolean, defaultValue: false},
-	  { name: 'help', alias: 'h', type: Boolean, defaultValue: false }
+	{ name: 'imagedir', alias: 'd', type: String, defaultValue: './images' },
+	{ name: 'saveimagedata', alias: 's', type: Boolean, defaultValue: false },
+	{ name: 'port', alias: 'p', type: Number },
+	{ name: 'help', alias: 'h', type: Boolean, defaultValue: false }
 ],
-listenPort = process.env.PORT || 3100,
 commandLineUsage = require('command-line-usage'),
 sections = [
   {
@@ -26,9 +26,15 @@ sections = [
 		alias: optionDefinitions[1].alias,
         description: 'Save images with metadata from detection processing.'
       },
-      {
+	  {
         name: optionDefinitions[2].name,
 		alias: optionDefinitions[2].alias,
+		typeLabel: '{underline port number}',
+        description: 'Sets the port number for the server to use. If not used, the PORT environment variable is used. If that is not defined, the server defaults to port 3100.'
+      },
+      {
+        name: optionDefinitions[3].name,
+		alias: optionDefinitions[3].alias,
         description: 'Print this usage guide.'
       }
     ]
@@ -36,6 +42,11 @@ sections = [
 ],
 usage = commandLineUsage(sections),
 options = commandLineArgs(optionDefinitions);
+
+var listenPort = process.env.PORT || 3100;
+
+if (options.port)
+	listenPort = options.port;
 
 if (options.help)
 	console.log(usage);
